6a07db2bd3cbc4990f2a98f923c6ffc6ba44cf65
Nolan Bradshaw <nolanbradshaw22@gmail.com>
2021-11-10 18:15:14 -0500

    Abstracting the client behind an interface (#17)

---
+++ NHL.NET/INHLClient.cs
@@ -0,0 +1,17 @@
+﻿using NHL.NET.Endpoints.Conference;
+using NHL.NET.Endpoints.Division;
+using NHL.NET.Endpoints.Franchise;
+using NHL.NET.Endpoints.Standings;
+using NHL.NET.Endpoints.Team;
+
+namespace NHL.NET
+{
+    public interface INHLClient
+    {
+        ITeamEndpoint Teams { get; }
+        IFranchiseEndpoint Franchises { get; }
+        IDivisionEndpoint Divisions { get; }
+        IStandingsEndpoints Standings { get; set; }
+        IConferenceEndpoints Conferences { get; set; }
+    }
+}
--- NHL.NET/NHLClient.cs
+++ NHL.NET/NHLClient.cs
@@ -4,10 +4,11 @@ using NHL.NET.Endpoints.Franchise;
 using NHL.NET.Endpoints.Standings;
 using NHL.NET.Endpoints.Team;
 using NHL.NET.Http;
+using NHL.NET.Http.Interfaces;
 
 namespace NHL.NET
 {
-    public class NHLClient
+    public class NHLClient : INHLClient
     {
         public ITeamEndpoint Teams { get; }
         public IFranchiseEndpoint Franchises { get; }
@@ -19,6 +20,15 @@ namespace NHL.NET
         {
             var requester = new Requester();
 
+            Franchises = new FranchiseEndpoint(requester);
+            Teams = new TeamEndpoint(requester);
+            Divisions = new DivisionEndpoint(requester);
+            Standings = new StandingsEndpoints(requester);
+            Conferences = new ConferenceEndpoints(requester);
+        }
+
+        public NHLClient(IRequester requester)
+        {
             Franchises = new FranchiseEndpoint(requester);
             Teams = new TeamEndpoint(requester);
             Divisions = new DivisionEndpoint(requester);
